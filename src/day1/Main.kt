c691e2006e554d018bd7bb13eb807040dd17ad5e
jpivi <pivijulien@gmail.com>
2023-12-26 22:21:20 +0100

    Day1-part1

---
+++ src/day1/Main.kt
@@ -0,0 +1,19 @@
+package day1
+
+import java.io.File
+
+fun main() {
+
+    var totalEfficient = 0
+    File("C:\\dev\\AdventOfCode\\resources\\day1\\input.txt").forEachLine {
+        //println(it)
+        val first : Char? =  it.find{ c: Char -> c.isDigit()}
+        val last : Char? = it.findLast{ c: Char -> c.isDigit()}
+
+        val fusion: String = first.toString().plus(last)
+        totalEfficient += fusion.toInt()
+    }
+
+    println(totalEfficient);
+
+}
